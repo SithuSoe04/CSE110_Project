081599fd7730fd04c3c00b38e5e83dddf8fa2786
SithuSoe04 <63730799+SithuSoe04@users.noreply.github.com>
2024-11-01 19:58:13 -0700

    Implemented Navbar

---
+++ tritoncircles/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Clubs from './views/Clubs';
+import Events from './views/Events';
+import Friends from './views/Friends';
+import Home from './views/Home';
+import Profile from './views/Profile';
+import Calendar from './views/Calendar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="events" element={<Events/>} />
+       <Route path="clubs" element={<Clubs />} />
+       <Route path="calendar" element={<Calendar/>} />
+       <Route path="friends" element={<Friends />} />
+       <Route path="profile" element={<Profile />} />
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ tritoncircles/src/views/Clubs.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Clubs = () => {
+    return <h1>Clubs Page</h1>
+}
+
+export default Clubs;
